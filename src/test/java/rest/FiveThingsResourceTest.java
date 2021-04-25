0e23f8219872814de0dda112d555310f8830795e
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-04-25 23:07:16 +0200

    JJ added integrationtests and a few unittests + rolesallowed on 5things endpoint

--- src/test/java/rest/FiveThingsResourceTest.java
+++ src/test/java/rest/FiveThingsResourceTest.java
@@ -1,6 +1,5 @@
 package rest;
 
-import entities.RenameMe;
 import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
@@ -22,11 +21,10 @@ import org.junit.jupiter.api.Test;
 //Uncomment the line below, to temporarily disable this test
 @Disabled
 
-public class RenameMeResourceTest {
+public class FiveThingsResourceTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/ca3/api";
-    private static RenameMe r1, r2;
 
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
@@ -64,13 +62,10 @@ public class RenameMeResourceTest {
     @BeforeEach
     public void setUp() {
         EntityManager em = emf.createEntityManager();
-        r1 = new RenameMe("Some txt", "More text");
-        r2 = new RenameMe("aaa", "bbb");
+
         try {
             em.getTransaction().begin();
-            em.createNamedQuery("RenameMe.deleteAllRows").executeUpdate();
-            em.persist(r1);
-            em.persist(r2);
+           
             em.getTransaction().commit();
         } finally {
             em.close();
